f9d40193b1ce3784b5501c5e38559ee864f74df1
EmersonGald1n0 <rm93899@fiap.com.br>
2022-08-28 21:08:14 -0300

    importando componentes

--- sprint03/src/App.jsx
+++ sprint03/src/App.jsx
@@ -16,16 +16,16 @@ export default function App() {
   return (
     <>
       <Navbar />
-        <Container customClass="min-height">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/servicos" element={<Servicos />} />
-          </Routes>
-        </Container>
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/servicos" element={<Servicos />} />
+        </Routes>
+      </Container>
       <Footer />
     </>
   )
