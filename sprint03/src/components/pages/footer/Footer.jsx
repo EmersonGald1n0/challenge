61db76f2dd322e60381cc0b6310784820ffb24d3
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-11 12:06:25 -0300

    alterado css do rodapé e add span (azul)

--- sprint03/src/components/pages/footer/Footer.jsx
+++ sprint03/src/components/pages/footer/Footer.jsx
@@ -6,23 +6,25 @@ import styles from './Footer.module.css'
 export default function Footer() {
 
   return (
-    <footer className={styles.footer}>
-      <ul className={styles.social_list}>
-        <li>
-          <FaFacebook />
-        </li>
-        <li>
-          <FaInstagram />
-        </li>
-        <li>
-          <FaLinkedin />
-        </li>
-      </ul>
-      <p className={styles.copyright}>
-        &copy; Copyright 2022 - Todos os direitos reservados - Desenvolvido por <span className={styles.copyright}>Art Labs Soluções
-          Digitais</span>
-      </p>
-    </footer>
+    <div className={styles.rodape}>
+      <footer className={styles.footer}>
+        <ul className={styles.social_list}>
+          <li>
+            <FaFacebook />
+          </li>
+          <li>
+            <FaInstagram />
+          </li>
+          <li>
+            <FaLinkedin />
+          </li>
+        </ul>
+        <p className={styles.copyright}>
+          &copy; Copyright 2022 - Todos os direitos reservados - Desenvolvido por <span className={styles.copyright}>Art Labs Soluções
+            Digitais</span>
+        </p>
+      </footer>
+    </div>
 
   )
 }
