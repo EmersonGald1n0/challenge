1cae116485f0d626a3f451daef0419db5f523a86
EmersonGald1n0 <rm93899@fiap.com.br>
2022-09-11 12:04:06 -0300

    estrutura home modificada

---
+++ sprint03/src/components/pages/home/Item.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background-color: #efefef;
+  margin: 0 100px;
+`;
